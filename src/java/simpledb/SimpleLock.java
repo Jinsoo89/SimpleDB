2cbbfc5ab74761cccbe1b30c9153a8f615b46d17
Jinsoo Choi <hello.python89@gmail.com>
2019-02-27 18:49:05 -0800

    edit some comments

--- src/java/simpledb/SimpleLock.java
+++ src/java/simpledb/SimpleLock.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.*;
 
+// SimpleLock is a class that represents a lock in SimpleDB
 public class SimpleLock {
     // fields of SimpleLock
     private LockType type;
@@ -21,6 +22,7 @@ public class SimpleLock {
         this.dependencyGraph = dependencyGraph;
     }
 
+    // get owners of this lock
     public HashSet<TransactionId> getOwners() {
         return owns;
     }
@@ -29,34 +31,43 @@ public class SimpleLock {
         return owns.contains(tid);
     }
 
+    // release lock that associates with transaction
     public void release(TransactionId tid) {
+        // nothing happens when the lock type is INIT
         if (!owns.contains(tid) || type == LockType.INIT) {
             return;
         }
+        // release exclusive lock, make owns empty (one transaction held)
         if (type == LockType.EXCLUSIVE) {
             owns.clear();
+        // release shared lock, remove only the associated transaction in owns
         } else if (type == LockType.SHARED) {
             owns.remove(tid);
         }
+        // owns is empty, now the type of lock is INIT
         if (owns.isEmpty()) {
             type = LockType.INIT;
         }
         
+        // remove the associated transaction in waitlist
         for (TransactionId wait : waits) {
             ArrayList<TransactionId> waitlist = dependencyGraph.get(wait);
             waitlist.remove(tid);
         }
         
+        // wakes up all thread, which are waiting
         condition.signalAll();
     }
 
+    // check if shared lock can be granted
     public boolean isShared(TransactionId tid) {
         if (type == LockType.EXCLUSIVE) {
-            return owns.contains(tid); // Can grant shared lock if owns exclusive.
+            return owns.contains(tid);
         }
         return true;
     }
-
+    
+    // check if exclusive lock can be granted
     public boolean isExclusive(TransactionId tid) {
         if (type == LockType.EXCLUSIVE) {
             return owns.contains(tid);
@@ -70,7 +81,9 @@ public class SimpleLock {
         return true;
     }
 
+    // acquire shared lock
     public boolean acquireShared(TransactionId tid) {
+        // check availability
         if (!isShared(tid)) {
             return false;
         }
@@ -93,7 +106,9 @@ public class SimpleLock {
         return true;
     }
 
+    // acquire exclusive lock
     public boolean acquireExclusive(TransactionId tid) {
+     // check availability
         if (!isExclusive(tid)) {
             return false;
         }
@@ -116,33 +131,35 @@ public class SimpleLock {
         }
         return true;
     }
-
-    public void wait(boolean shared, TransactionId tid) throws TransactionAbortedException {
+    
+    // wait method is used in LockManager when a lock acquiring request occurs
+    public void wait(boolean isShared, TransactionId tid) throws TransactionAbortedException {
         if (!waits.contains(tid)) {
             waits.add(tid);
-            
             ArrayList<TransactionId> waitlist = dependencyGraph.get(tid);
             
             if (waitlist == null) {
                 waitlist = new ArrayList<TransactionId>();
                 dependencyGraph.put(tid, waitlist);
             }
+            
             for (TransactionId own : owns) {
                 waitlist.add(own);
             }
         }
         
         try {
-            if (shared) {
+            // the type of lock is a shared lock
+            if (isShared) {
                 while (!isShared(tid)) {
-                    condition.await(1, TimeUnit.SECONDS);
+                    condition.await(100, TimeUnit.MILLISECONDS);
                 }
+            // the type of lock is a exclusive lock
             } else {
                 while (!isExclusive(tid)) {
-                    condition.await(1, TimeUnit.SECONDS);
+                    condition.await(100, TimeUnit.MILLISECONDS);
                 }
             }
-            
         } catch (InterruptedException e) {
             e.printStackTrace();
             throw new TransactionAbortedException();
